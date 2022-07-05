8bbcc775088bee4c0fd9bd81c92967b4d35896ae
qiuyu1995 <gu.675@osu.edu>
2022-07-05 14:25:48 -0700

    Initial commit

--- R/main.R
+++ R/main.R
@@ -1,20 +1,20 @@
 #' Path-following algorithm for L2 penalized nonsmooth problems
-#' 
-#' @description 
-#' Compute exact solution path for case-weight adjusted problem 
+#'
+#' @description
+#' Compute exact solution path for case-weight adjusted problem
 #' \loadmathjax
 #' \mjseqn{(\beta_{0,w}, \beta_{w}) = argmin_{\beta_0, \beta} \sum_{i \neq j} f(g_i(\beta_0, \beta)) + w*f(g_{j}(\beta_0, \beta)) + \lambda / 2 * \|\beta\|_2^2}
 #' for \mjseqn{0 <= w <= 1}, where \mjseqn{g_i(\beta_0, \beta) = a_i \beta_0 + b_i^T \beta + c_i} and \mjseqn{f(r) = \alpha_0 \max(r, 0) + \alpha_1 \max(-r, 0).}
-#' The exact solution path is shown to be piece-wise linear 
+#' The exact solution path is shown to be piece-wise linear
 #' and the function will return all the breakpoints and their solutions.
-#' 
+#'
 #' @param X A \eqn{n \times p} feature matrix
 #' @param Y A \eqn{n \times 1} response vector
 #' @param lam Regularization parameter for L2 penalty
 #' @param obs_index Index of the observation that is attached a weight, ranging from 1 to n
 #' @param class Specify the problem class with default value "quantile". Use "quantile" for quantile regression, and "svm" for support vector machine
 #' @param tau The parameter for quantile regression, ranging between 0 and 1. No need to specify the value of it if choose class = "svm"
-#' 
+#'
 #' @return W_vec A list of breakout points
 #' @return Beta_0 True values of beta_{0,w} at breakout points
 #' @return Beta True values of beta_{w} at breakout points
@@ -43,7 +43,7 @@ nonsmooth_path <- function(X, Y, lam, obs_index, class = "quantile", tau = 0.5){
     cat("\n The current version only supports 'quantile' and 'svm' class options.\n")
     return(0)
   }
-  
+
   ## Compute the full-data solution
   beta <- CVXR::Variable(p)
   beta_0 = CVXR::Variable()
@@ -61,34 +61,34 @@ nonsmooth_path <- function(X, Y, lam, obs_index, class = "quantile", tau = 0.5){
 }
 
 #' Case influence assessment for L2 penalized nonsmooth models
-#' 
-#' @description 
+#'
+#' @description
 #' Compute the global influence, which is based on case influence graph, for each case under \eqn{\ell_2} regularized nonsmooth
-#' models, including quantile regression and svm. To compute the case influence graph for each case, we need to solve a solution path problem.  
+#' models, including quantile regression and svm. To compute the case influence graph for each case, we need to solve a solution path problem.
 #' In our algorithm, the exact path can be computed and it is piece-wise linear.
-#' 
+#'
 #' @param X A \eqn{n \times p} feature matrix
 #' @param Y A \eqn{n \times 1} response vector
 #' @param lam Regularization parameter for L2 penalty
 #' @param class Specify the problem class with default value "quantile". Use "quantile" for quantile regression, and "svm" for support vector machine
 #' @param tau The parameter for quantile regression, ranging between 0 and 1. No need to specify the value of it if choose class = "svm"
 #' @param influence_measure Specify the influence measure for computing global influence. For classification problem, "FMD" represents
-#' functional margin difference and "BDLD" represents Binomial deviance loss difference. The default is "FMD". For regression problem, no 
+#' functional margin difference and "BDLD" represents Binomial deviance loss difference. The default is "FMD". For regression problem, no
 #' need to specify the influence_measure
-#' 
+#'
 #' @return global_influence Global influence for each case
 #' @return cook_distance Cook's distance for each case
 #' @export
-#' 
+#'
 Compute_CaseInflu_nonsmooth <- function(X, Y, lam, class = "svm", tau = 0.5, influence_measure = "FMD"){
   n = dim(X)[1]
   p = dim(X)[2]
-  
+
   if (influence_measure != "FMD" & influence_measure != "BDLD") {
     cat("\n The current version only supports 'BDLD' and 'FMD' influence measure options.\n")
     return(0)
   }
-  
+
   if (class == "quantile") {
     a = -rep(1, n)
     B = -X
@@ -109,7 +109,7 @@ Compute_CaseInflu_nonsmooth <- function(X, Y, lam, class = "svm", tau = 0.5, inf
     cat("\n The current version only supports 'quantile' and 'svm' class options.\n")
     return(0)
   }
-  
+
   ## Compute the full-data solution
   beta <- CVXR::Variable(p)
   beta_0 = CVXR::Variable()
@@ -126,31 +126,31 @@ Compute_CaseInflu_nonsmooth <- function(X, Y, lam, class = "svm", tau = 0.5, inf
 }
 
 #' Local influence assessment for L2 penalized nonsmooth models (svm + quantile regression)
-#' 
-#' @description 
+#'
+#' @description
 #' Compute the local influence for each case under \eqn{\ell_2} regularized nonsmooth models, including quantile regression and svm.
-#' 
+#'
 #' @param X A \eqn{n \times p} feature matrix
 #' @param Y A \eqn{n \times 1} response vector
 #' @param lam Regularization parameter for L2 penalty
 #' @param class Specify the problem class with default value "quantile". Use "quantile" for quantile regression, and "svm" for support vector machine
 #' @param tau The parameter for quantile regression, ranging between 0 and 1. No need to specify the value of it if choose class = "svm"
 #' @param influence_measure Specify the influence measure for computing global influence. For classification problem, "FMD" represents
-#' functional margin difference and "BDLD" represents Binomial deviance loss diference. The default is "FMD". For regression problem, no 
+#' functional margin difference and "BDLD" represents Binomial deviance loss diference. The default is "FMD". For regression problem, no
 #' need to specify the influence_measure
-#' 
+#'
 #' @return LocalInfluence_vec Local influence for each case
 #' @export
-#' 
+#'
 Compute_LocalInflu_nonsmooth <- function(X, Y, lam, class = "svm", tau = 0.5, influence_measure = "FMD"){
   n = dim(X)[1]
   p = dim(X)[2]
-  
+
   if (influence_measure != "FMD" & influence_measure != "BDLD") {
     cat("\n The current version only supports 'BDLD' and 'FMD' influence measure options.\n")
     return(0)
   }
-  
+
   if (class == "quantile") {
     a = -rep(1, n)
     B = -X
@@ -171,7 +171,7 @@ Compute_LocalInflu_nonsmooth <- function(X, Y, lam, class = "svm", tau = 0.5, in
     cat("\n The current version only supports 'quantile' and 'svm' class options.\n")
     return(0)
   }
-  
+
   ## Compute the full-data solution
   beta <- CVXR::Variable(p)
   beta_0 = CVXR::Variable()
@@ -188,15 +188,15 @@ Compute_LocalInflu_nonsmooth <- function(X, Y, lam, class = "svm", tau = 0.5, in
 }
 
 #' Path-following algorithm for L2 penalized Generalized linear models
-#' 
-#' @description 
+#'
+#' @description
 #' Compute the global influence, which is based on case influence graph, for each case under \eqn{\ell_2} regularized Generalized linear
-#' Models. To compute the case influence graph for each case, we need to solve the following solution path problem: 
+#' Models. To compute the case influence graph for each case, we need to solve the following solution path problem:
 #' \loadmathjax
 #' \mjdeqn{\theta(t) = argmin_{\theta}  e^{-t} L_{j}(\theta) + \sum_{i \neq j} L_{i}(\theta) + \lambda / 2 * \|\theta\|_2^2 \text{ for } t \in [0, t_{max}]}{ASCII representation}
 #' In our algorithm, the grid points are adaptively selected to save computation while maintaining the accuracy of the entire path.
 #' At each grid point, the optimization problem can be solved by either Newton method or gradient descent method.
-#' 
+#'
 #' @param X A \eqn{n \times p} feature matrix
 #' @param Y A \eqn{n \times 1} response vector
 #' @param lam Regularization parameter for L2 penalty
@@ -205,21 +205,21 @@ Compute_LocalInflu_nonsmooth <- function(X, Y, lam, class = "svm", tau = 0.5, in
 #' @param method Specify the method for computing the solution path. Default is "Newton". Use "GD" for gradient descent
 #' @param t_max Range of solution path \eqn{[0, t_{\max}]}
 #' @param class Specify the problem class with default value "logistic". Use "logistic" for logistic regression, and "poisson" for poisson regression
-#' 
+#'
 #' @return t_vec Adaptively selected grid points
 #' @return theta Generated solution path at grid points
 #' @return alpha_t Step size between grid points
 #' @export
-#' 
+#'
 smooth_path <- function(X, Y, lam, obs_index, epsilon, method = "Newton", t_max = 10, class = "logistic"){
   n = dim(X)[1]
   p = dim(X)[2]
   L_vec = rep(0, n)
-  
+
   if (class == "logistic") {
     for (i in 1:n)
       L_vec[i] = norm(X[i,], '2') ** 2 / (4*n)
-    
+
     ###CVX calculates the full data solution
     beta <- CVXR::Variable(p)
     obj = 1/n*(CVXR::sum_entries(CVXR::logistic(X[Y == -1, ] %*% beta))+CVXR::sum_entries(CVXR::logistic(-X[Y == 1, ] %*% beta)))+lam/2*CVXR::power(CVXR::norm2(beta), 2)
@@ -269,17 +269,17 @@ smooth_path <- function(X, Y, lam, obs_index, epsilon, method = "Newton", t_max 
   else {
     cat("\n The current version only supports 'logistic' and 'poisson' class options.\n")
   }
-  
+
 }
 
 #' Case influence assessment for L2 penalized GLM
-#' 
-#' @description 
+#'
+#' @description
 #' Compute the global influence, which is based on case influence graph, for each case under \eqn{\ell_2} regularized logistic
-#' regression. To compute the case influence graph for each case, we need to solve a solution path problem. In our algorithm, 
+#' regression. To compute the case influence graph for each case, we need to solve a solution path problem. In our algorithm,
 #' the grid points are adaptively selected to save computation while maintaining the accuracy of the entire path. At each grid point,
-#' the optimization problem can be solved by either Newton method or gradient descent method. 
-#' 
+#' the optimization problem can be solved by either Newton method or gradient descent method.
+#'
 #' @param X A \eqn{n \times p} feature matrix
 #' @param Y A \eqn{n \times 1} response vector
 #' @param lam Regularization parameter for L2 penalty
@@ -288,28 +288,28 @@ smooth_path <- function(X, Y, lam, obs_index, epsilon, method = "Newton", t_max 
 #' @param class Specify the problem class with default value "logistic". Use "logistic" for logistic regression, and "poisson" for poisson regression
 #' @param method Specify the method for computing the solution path. Default is "Newton". Use "GD" for gradient descent
 #' @param influence_measure Specify the influence measure for computing global influence. For classification problem, "FMD" represents
-#' functional margin difference and "BDLD" represents Binomial deviance loss diference. The default is "FMD". For regression problem, no 
+#' functional margin difference and "BDLD" represents Binomial deviance loss diference. The default is "FMD". For regression problem, no
 #' need to specify the influence_measure
-#' 
+#'
 #' @return global_influence Global influence for each case
 #' @return cook_distance Cook's distance for each case
 #' @export
-#' 
+#'
 Compute_CaseInflu_GLM <- function(X, Y, lam, epsilon, t_max = 10, class = "logistic", method = "Newton", influence_measure = "FMD"){
   n = dim(X)[1]
   p = dim(X)[2]
-  
+
   ###Input check
   if (method != "Newton" & method != "GD") {
     cat("\n The current version only supports 'GD' and 'Newton' method options.\n")
     return(0)
   }
-  
+
   if (influence_measure != "FMD" & influence_measure != "BDLD") {
     cat("\n The current version only supports 'BDLD' and 'FMD' influence measure options.\n")
     return(0)
   }
-  
+
   if (class == "logistic") {
     ###CVX calculates the full data solution
     beta <- CVXR::Variable(p)
@@ -334,31 +334,31 @@ Compute_CaseInflu_GLM <- function(X, Y, lam, epsilon, t_max = 10, class = "logis
 }
 
 #' LOO solution computation for L2 penalized GLM
-#' 
-#' @description 
+#'
+#' @description
 #' Compute the LOO solution based on the one-step Newton method, using the solution at the last grid point as the initialization
-#' 
+#'
 #' @param X A \eqn{n \times p} feature matrix
 #' @param Y A \eqn{n \times 1} response vector
 #' @param lam Regularization parameter for L2 penalty
-#' @param obs_index Index of the observation that is left out 
+#' @param obs_index Index of the observation that is left out
 #' @param epsilon Pre-specified error tolerance of the entire path
 #' @param t_max Range of solution path \eqn{[0, t_{\max}]}
 #' @param class Specify the problem class with default value "logistic". Use "logistic" for logistic regression, and "poisson" for poisson regression
 #' @param method Specify the method for computing the solution path. Default is "Newton". Use "GD" for gradient descent
-#' 
+#'
 #' @return The computed LOO solution
 #' @export
-#' 
+#'
 LOO_GLM <- function(X, Y, lam, obs_index, epsilon, t_max = 10, class = "logistic", method = "Newton"){
   n = dim(X)[1]
   p = dim(X)[2]
   L_vec = rep(0, n)
-  
+
   if (class == "logistic") {
     for (i in 1:n)
       L_vec[i] = norm(X[i,], '2') ** 2 / (4*n)
-    
+
     ###CVX calculates the full data solution
     beta <- CVXR::Variable(p)
     obj = 1/n*(CVXR::sum_entries(CVXR::logistic(X[Y == -1, ] %*% beta))+CVXR::sum_entries(CVXR::logistic(-X[Y == 1, ] %*% beta)))+lam/2*CVXR::power(CVXR::norm2(beta), 2)
@@ -410,6 +410,54 @@ LOO_GLM <- function(X, Y, lam, obs_index, epsilon, t_max = 10, class = "logistic
     }
     ## Run LOO
     .Call(`_Caseinflu_poisson_compute_LOO`, X, Y, path[[1]][, ncol(path[[1]])], lam, obs_index-1)
+  }
+  else {
+    cat("\n The current version only supports 'logistic' and 'poisson' class options.\n")
+  }
+}
+
+#' Local influence assessment for L2 penalized GLM
+#'
+#' @description
+#' Compute the local influence for each case under \eqn{\ell_2} regularized GLM.
+#'
+#' @param X A \eqn{n \times p} feature matrix
+#' @param Y A \eqn{n \times 1} response vector
+#' @param lam Regularization parameter for L2 penalty
+#' @param class Specify the problem class with default value "quantile". Use "quantile" for quantile regression, and "svm" for support vector machine
+#' @param influence_measure Specify the influence measure for computing local influence. For classification problem, "FMD" represents
+#' functional margin difference and "BDLD" represents Binomial deviance loss diference. The default is "FMD". For regression problem, no
+#' need to specify the influence_measure
+#'
+#' @return LocalInfluence_vec Local influence for each case
+#' @export
+#'
+Compute_LocalInflu_GLM <- function(X, Y, lam, class = "logistic", influence_measure = "FMD"){
+  n = dim(X)[1]
+  p = dim(X)[2]
+
+  if (influence_measure != "FMD" & influence_measure != "BDLD") {
+    cat("\n The current version only supports 'BDLD' and 'FMD' influence measure options.\n")
+    return(0)
+  }
+
+  if (class == "logistic") {
+    ###CVX calculates the full data solution
+    beta <- CVXR::Variable(p)
+    obj = 1/n*(CVXR::sum_entries(CVXR::logistic(X[Y == -1, ] %*% beta))+CVXR::sum_entries(CVXR::logistic(-X[Y == 1, ] %*% beta)))+lam/2*CVXR::power(CVXR::norm2(beta), 2)
+    prob <- CVXR::Problem(Minimize(obj))
+    result <- CVXR::solve(prob)
+    Beta_0 <- result$getValue(beta)
+    .Call(`_Caseinflu_LocalInfluence_logistic`, X, Y, Beta_0, lam, influence_measure)
+  }
+  else if (class == "poisson") {
+    ###CVX calculates the full data solution
+    beta <- CVXR::Variable(p)
+    obj = 1/n*CVXR::sum_entries(exp(X %*% beta) - CVXR::multiply(Y, X %*% beta))+lam/2*CVXR::power(CVXR::norm2(beta), 2)
+    prob <- CVXR::Problem(Minimize(obj))
+    result <- CVXR::solve(prob)
+    Beta_0 <- result$getValue(beta)
+    .Call(`_Caseinflu_LocalInfluence_poisson`, X, Y, Beta_0, lam)
   }
   else {
     cat("\n The current version only supports 'logistic' and 'poisson' class options.\n")
